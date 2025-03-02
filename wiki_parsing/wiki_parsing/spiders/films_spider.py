ff7b751f0bc06f3be20fd0a490f1519b1bd46740
Arthur <relentlessviper10@gmail.com>
2025-03-02 22:43:10 +0300

    Finish Scrapy spider implementation & data parsing from Wiki.

--- wiki_parsing/wiki_parsing/spiders/films_spider.py
+++ wiki_parsing/wiki_parsing/spiders/films_spider.py
@@ -1,20 +1,42 @@
-# Import necessary dependencies
 import scrapy
-from pathlib import Path
+from urllib.parse import urljoin
 
 class FilmsSpider(scrapy.Spider):
-    name = "films"
+    name = "films_v2"
+    start_urls = [
+        "https://en.wikipedia.org/wiki/List_of_highest-grossing_films"
+    ]
 
-    def start_requests(self):
-        urls = [
-            "https://en.wikipedia.org/wiki/List_of_highest-grossing_films"
-        ]
-
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-        
     def parse(self, response):
-        page_name = response.url.split("/")[-1]
-        filename = f"films-{page_name}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file: {filename}")
+        # Extract all film links from the second table with films by year
+        table = response.xpath('//table[contains(@class, "wikitable plainrowheaders")]')[1]
+        rows = table.xpath(".//tr")
+        film_links = []
+        for row in rows:
+            res = row.xpath('.//td//i//a//@href').getall()
+            if len(res) == 0:
+                continue
+            else:
+                film_links.append(res[0])
+
+        # Get data from each film link
+        for link in film_links:
+            absolute_url = urljoin("https://en.wikipedia.org", link)
+            yield scrapy.Request(url=absolute_url, callback=self.parse_film_page)
+
+    def parse_film_page(self, response):
+        info_table = response.xpath('//table[contains(@class, "infobox")]')
+        data = {
+            "title": info_table.xpath('.//th[contains(@class, "infobox-above")]//text()').get(),
+            "directed_by": info_table.xpath('.//th[contains(text(), "Directed by")]/following-sibling::td//text()').getall(),
+            "screenplay_by": info_table.xpath('.//th[contains(text(), "Screenplay by")]/following-sibling::td//text()').getall(),
+            "based_on": info_table.xpath('.//th[contains(text(), "Based on")]/following-sibling::td//text()').getall(),
+            "produced_by": info_table.xpath('.//th[contains(text(), "Produced by")]/following-sibling::td//text()').getall(),
+            "starring": info_table.xpath('.//th[contains(text(), "Starring")]/following-sibling::td//text()').getall(),
+            "release_date": info_table.xpath('.//th[contains(text(), "Release date")]/following-sibling::td//text()').getall(),
+            "country": info_table.xpath('.//th[contains(text(), "Country")]/following-sibling::td//text()').getall(),
+            "budget": info_table.xpath('.//th[contains(text(), "Budget")]/following-sibling::td//text()').getall(),
+            "box_office": info_table.xpath('.//th[contains(text(), "Box office")]/following-sibling::td//text()').getall(),
+        }
+
+        yield data
